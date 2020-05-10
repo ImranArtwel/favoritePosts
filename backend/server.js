0e42fb029eaee29e7a47be46c1ffefd093affcbe
artwel <imranartweljunior@gmail.com>
2020-05-09 23:46:11 -0500

    finalize heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -21,6 +22,16 @@ const postsRouter = require("./routes/posts");
 const usersRouter = require("./routes/users");
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
